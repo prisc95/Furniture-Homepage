6155b3948d5ad4a8ee49b8134b6e91b350acd00c
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-06 14:02:23 +0200

    Added pricing html blade

---
+++ resources/views/partials/pageblocks/pricing.blade.php
@@ -0,0 +1,30 @@
+<section class="pricing">
+    <div class="top">
+        <img src="wp-content\themes\sage\resources\images\furniture2.png" alt="">
+        <div class="top-content">
+            <h1>Maak je klaar voor Kerstmis</h1>
+            <button>Huidige trends</button>
+        </div>
+        <div class="cards">
+            <div class="card">
+                <div class="icons">
+                    <i class="fa-solid fa-link"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-magnifying-glass-plus"></i>
+                </div>
+                <div class="img-wrapper">
+                    <img src="wp-content\themes\sage\resources\images\pricing-armchair.png" alt="">
+                </div>
+                <h3>Fauteuil</h3>
+                <div class="stars">
+                    <i class="fa-solid fa star"></i>
+                    <i class="fa-solid fa star"></i>
+                    <i class="fa-solid fa star"></i>
+                    <i class="fa-solid fa star"></i>
+                    <i class="fa-solid fa star"></i>
+                </div>
+                <h4>€99,00</h4>
+            </div>
+        </div>
+    </div>
+</section>
