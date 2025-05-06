c0cb38bb4119f2e5874c8dcbc69f0fe39f1c89ae
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-06 13:54:50 +0200

    Added products html blade

---
+++ resources/views/partials/pageblocks/products.blade.php
@@ -0,0 +1,33 @@
+<section class="products">
+    <div class="top">
+        <h3>Zoeken naar</h3>
+        <h2>15000</h2>
+        <h1>Producten</h1>
+    </div>
+    <div class="icons">
+        <div class="bedroom">
+            <img src="wp-content\themes\sage\resources\images\bedroom.png" alt="">
+            <span>Slaapkamer</span>
+        </div>
+        <div class="buffet">
+            <img src="wp-content\themes\sage\resources\images\buffet.png" alt="">
+            <span>Tv-meubelen</span>
+        </div>
+        <div class="dining">
+            <img src="wp-content\themes\sage\resources\images\dining.png" alt="">
+            <span>Koken & eten</span>
+        </div>
+        <div class="sofa">
+            <img src="wp-content\themes\sage\resources\images\sofa.png" alt="">
+            <span>Banken & fauteuils</span>
+        </div>
+        <div class="table">
+            <img src="wp-content\themes\sage\resources\images\table.png" alt="">
+            <span>Ladekasten</span>
+        </div>
+        <div class="wardrobe">
+            <img src="wp-content\themes\sage\resources\images\wardrobe.png" alt="">
+            <span>Kledingkasten</span>
+        </div>
+    </div>
+</section>
