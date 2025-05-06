23885e061d3d9e9c0ef0d08068d39fae3f8d87ff
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-06 14:05:31 +0200

    Added blog html blade

---
+++ resources/views/partials/pageblocks/blog.blade.php
@@ -0,0 +1,27 @@
+<section class="blog">
+    <div class="content">
+        <div class="top">
+            <div class="magazine">
+                <img src="wp-content\themes\sage\resources\images\magazine.png" alt="">
+            </div>
+            <div class="trends">
+                <h1>Winter trends 2025</h1>
+                <button>Download</button>
+            </div>
+        </div>
+        <div class="articles">
+            <div class="article">
+                <span>01</span>
+                <span>Lorem ipsum dolor sit amet</span>
+            </div>
+            <div class="article">
+                <span>02</span>
+                <span>Lorem ipsum dolor sit amet</span>
+            </div>
+            <div class="article">
+                <span>03</span>
+                <span>Lorem ipsum dolor sit amet</span>
+            </div>
+        </div>
+    </div>
+</section>
