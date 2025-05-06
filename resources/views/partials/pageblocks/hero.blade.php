1f323108ee803142366dd946bbce83f37fe6010a
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-06 19:41:14 +0200

    Adjustments on layout hero

--- resources/views/partials/pageblocks/hero.blade.php
+++ resources/views/partials/pageblocks/hero.blade.php
@@ -1,9 +1,14 @@
 <section class="hero">
-    <div class="landing-bg"></div>
-    <div class="banner">
-        <img src="wp-content\themes\sage\resources\images\banner-img.jpg" alt="">
-        <h1>Luxe meubels</h1>
-        <button class="banner-btn 1">Ga naar winkel</button>
-        <button class="banner-btn 2">Ontdek</button>
+    <div class="bg"></div>
+    <div class="container">
+        <div class="content">
+            <h1>Luxe <span>meubels</span></h1>
+            <div class="btn-wrapper">
+                <button class="btn1">Ga naar winkel</button>
+                <button class="btn2">Ontdek</button>
+            </div>
+           
+        </div>
+       
     </div>
 </section>
