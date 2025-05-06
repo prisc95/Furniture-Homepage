44ff74e0ced63edfb01a03fa0e68cad96f3268d7
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-06 14:09:49 +0200

    Added footer html

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,10 @@
 <footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+  {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
+  <div class="copyright">
+    <p>&copy; All Rights Reserved 2025. Gemaakt door: Priscilla de Groot.</p>
+  </div>
+  <div class="brand">
+    <i class="fa-solid fa-couch"></i>
+    <h1>Meubels</h1>
+  </div>
 </footer>
