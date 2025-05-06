b07fe354a296c948c1075f6d7c009dceecd2aec2
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-06 14:07:45 +0200

    Added contact html blade

---
+++ resources/views/partials/pageblocks/contact.blade.php
@@ -0,0 +1,11 @@
+<section class="contact">
+    <div class="content">
+        <div class="left">
+            <h2>Abboneer voor de nieuwsbrief en krijg <span>30% korting</span></h2>
+        </div>
+        <div class="right">
+            <input type="email" placeholder="E-mailadres">
+            <button>Schrijf je in</button>
+        </div>
+    </div>
+</section>
