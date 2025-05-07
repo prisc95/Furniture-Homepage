509d2839166de2bd098e44213b18d6d057a04ff5
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-07 12:05:47 +0200

    styled pricing section

--- resources/views/partials/pageblocks/pricing.blade.php
+++ resources/views/partials/pageblocks/pricing.blade.php
@@ -1,11 +1,18 @@
 <section class="pricing">
-    <div class="top">
-        <img src="wp-content\themes\sage\resources\images\furniture2.png" alt="">
-        <div class="top-content">
-            <h1>Maak je klaar voor Kerstmis</h1>
-            <button>Huidige trends</button>
+    <div class="container">
+        <div class="top">
+            <div class="img-wrapper">
+                <img src="wp-content\themes\sage\resources\images\furniture2.png" alt="">
+            </div>
+            
+            <div class="content">
+                <h1>Maak je klaar voor Kerstmis</h1>
+                <button>Huidige trends</button>
+            </div>
         </div>
-        <div class="cards">
+
+        <div class="bottom">
+
             <div class="card">
                 <div class="icons">
                     <i class="fa-solid fa-link"></i>
@@ -15,16 +22,136 @@
                 <div class="img-wrapper">
                     <img src="wp-content\themes\sage\resources\images\pricing-armchair.png" alt="">
                 </div>
-                <h3>Fauteuil</h3>
+                <div class="content">
+                <h3>FAUTEUIL</h3>
                 <div class="stars">
-                    <i class="fa-solid fa star"></i>
-                    <i class="fa-solid fa star"></i>
-                    <i class="fa-solid fa star"></i>
-                    <i class="fa-solid fa star"></i>
-                    <i class="fa-solid fa star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
                 </div>
                 <h4>€99,00</h4>
+                </div>
             </div>
-        </div>
+       
+
+            <div class="card">
+                <div class="icons">
+                    <i class="fa-solid fa-link"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-magnifying-glass-plus"></i>
+                </div>
+                <div class="img-wrapper">
+                    <img src="wp-content\themes\sage\resources\images\pricing-couch.png" alt="">
+                </div>
+                <div class="content">
+                <h3>BANK</h3>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+                <h4>€129,00</h4>
+                </div>
+            </div>
+
+
+            <div class="card">
+                <div class="icons">
+                    <i class="fa-solid fa-link"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-magnifying-glass-plus"></i>
+                </div>
+                <div class="img-wrapper">
+                    <img src="wp-content\themes\sage\resources\images\pricing-lighting.png" alt="">
+                </div>
+                <div class="content">
+                <h3>HANGLAMP</h3>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+                <h4>€59,00</h4>
+                </div>
+            </div>
+
+
+            <div class="card">
+                <div class="icons">
+                    <i class="fa-solid fa-link"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-magnifying-glass-plus"></i>
+                </div>
+                <div class="img-wrapper">
+                    <img src="wp-content\themes\sage\resources\images\pricing-sofa.png" alt="">
+                </div>
+                <div class="content">
+                <h3>SOFA</h3>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+                <h4>€89,00</h4>
+                </div>
+            </div>
+
+
+            <div class="card">
+                <div class="icons">
+                    <i class="fa-solid fa-link"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-magnifying-glass-plus"></i>
+                </div>
+                <div class="img-wrapper">
+                    <img src="wp-content\themes\sage\resources\images\pricing-table.png" alt="">
+                </div>
+                <div class="content">
+                <h3>TAFEL</h3>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+                <h4>€49,00</h4>
+                </div>
+            </div>
+        
+
+            <div class="card">
+                <div class="icons">
+                    <i class="fa-solid fa-link"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-magnifying-glass-plus"></i>
+                </div>
+                <div class="img-wrapper">
+                    <img src="wp-content\themes\sage\resources\images\pricing-wardrobe.png" alt="">
+                </div>
+                <div class="content">
+                <h3>KLEDINGKAST</h3>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+                <h4>€599,00</h4>
+                </div>
+            </div>
+
     </div>
+
+    </div>
+
 </section>
