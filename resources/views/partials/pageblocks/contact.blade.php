e532fd9f31574fff726fb121003c0ba5a4f62c1b
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-07 12:06:13 +0200

    styled contact section

--- resources/views/partials/pageblocks/contact.blade.php
+++ resources/views/partials/pageblocks/contact.blade.php
@@ -1,11 +1,14 @@
 <section class="contact">
-    <div class="content">
+    <div class="container">
+
         <div class="left">
-            <h2>Abboneer voor de nieuwsbrief en krijg <span>30% korting</span></h2>
+            <h3>Abonneer voor de nieuwsbrief en krijg <span>30% korting</span></h3>
         </div>
+
         <div class="right">
             <input type="email" placeholder="E-mailadres">
             <button>Schrijf je in</button>
         </div>
+
     </div>
 </section>
